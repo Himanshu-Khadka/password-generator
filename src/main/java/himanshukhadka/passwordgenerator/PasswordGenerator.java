f8c3fc33135114704e3aa98f752433c470d62ad1
Himanshu-Khadka <iamhimanshu.me@gmail.com>
2024-05-27 16:55:39 -0600

    Fixed interface

--- src/main/java/himanshukhadka/passwordgenerator/PasswordGenerator.java
+++ src/main/java/himanshukhadka/passwordgenerator/PasswordGenerator.java
@@ -1,47 +1,100 @@
 package himanshukhadka.passwordgenerator;
 
 import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.layout.BorderPane;
-import javafx.scene.text.Text;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
 import javafx.stage.Stage;
 
+
+
 public class PasswordGenerator extends Application {
+    private int defaultLength = 8;
+
     @Override
     public void start(Stage stage){
-        int lengthOfPassword = 8;
+        int lengthOfPassword = defaultLength;
 
-        System.out.println(new password().generate(8,true));
-//        System.exit(1);
-
-        Text homeLabel = new Text("Password Generator");
-        Text passString = new Text();
-        passString.setVisible(false);
-        Label lengthLabel = new Label("Length");
-        TextField range = new TextField(Integer.toString(lengthOfPassword));
+        //Creating Label
+        Label mainLabel = new Label("Password Generator");
+        //Some fonts
+        Font font = Font.font("Times New Roman",FontWeight.EXTRA_BOLD,20);
+        Font font1 = Font.font("Roboto",FontWeight.BOLD,18);
+        mainLabel.setFont(font);
 
 
+        //Creating text fields
+        TextField passString = new TextField("Password Appears here");
+        TextField passLength = new TextField(Integer.toString(lengthOfPassword));
+        passString.setEditable(false);
+        passString.setFont(font1);
+        passString.setAlignment(Pos.CENTER);
+        passString.setPrefWidth(300);
+        passString.setPrefHeight(50);
+        passLength.setPrefWidth(50);
+
+
+        //Creating Buttons
         Button generateBtn = new Button("Generate");
-        generateBtn.setOnAction(e->{
-            passString.setText(new password().generate(lengthOfPassword,true));
-            passString.setVisible(true);
+        generateBtn.setStyle("-fx-pref-width: 100");
+        generateBtn.setStyle("-fx-border-radius: 20");
 
+        RadioButton specialCharacter = new RadioButton("Special Characters ");
+        specialCharacter.setSelected(true);
+        //On Button Pressed
+        generateBtn.setOnAction(e->{
+            String passStr;
+            if (specialCharacter.isSelected()){
+                passStr = new password().generate(lengthOfPassword,true);
+            }else {
+                passStr = new password().generate(lengthOfPassword,false);
+            }
+            passString.setText(passStr);
+            generateBtn.setText("Generate Next");
         });
 
+        //HBOx for main Label
+        HBox mainLabelHbox = new HBox(mainLabel);
+        mainLabelHbox.setAlignment(Pos.CENTER);
+        mainLabelHbox.setPadding(new Insets(20));
 
+        //HBox for label
+        HBox lengthBox = new HBox();
+        lengthBox.getChildren().addAll(new Label("Length:    "), passLength);
+        lengthBox.setAlignment(Pos.CENTER);
+
+        //Vbox for control pane
+        VBox controlPane = new VBox();
+        controlPane.setAlignment(Pos.CENTER);
+        controlPane.setPadding(new Insets(12,12,80,12));
+        controlPane.setSpacing(12);
+        controlPane.getChildren().addAll(lengthBox,specialCharacter,generateBtn);
+
+        //VBox for nodes
+        VBox Vpane = new VBox();
+        Vpane.setSpacing(20);
+        Vpane.setAlignment(Pos.CENTER);
+        Vpane.setPadding(new Insets(12,12,12,12));
+        Vpane.getChildren().addAll(passString);
+
+        //Main pane
         BorderPane pane = new BorderPane();
-        pane.setTop(homeLabel);
-        pane.setCenter(passString);
-        pane.setBottom(generateBtn);
+        pane.setTop(mainLabelHbox);
+        pane.setCenter(Vpane);
+        pane.setBottom(controlPane);
+        pane.setStyle("-fx-background-color: #F1F1F1");
 
-        Scene scene = new Scene(pane);
-        stage.setTitle("Password Generator");
+        //Setting up scene
+        Scene scene = new Scene(pane,500,350);
         stage.setScene(scene);
+        stage.setTitle("Password Generator");
         stage.show();
-
     }
 
 }
